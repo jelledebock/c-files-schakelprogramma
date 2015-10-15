6f739028f7895ebfe8470fbfc4463733069da9b3
Jelle De Bock <debockjelle@hotmail.com>
2015-10-15 16:07:39 +0200

    Only last exercise todo

--- Oefeningenles4/deeltal.h
+++ Oefeningenles4/deeltal.h
@@ -77,4 +77,72 @@ struct Deeltal * lees_deeltal(struct Deeltal *g)
 	return g;
 }
 
+struct Deeltal *lees_deeltallen(int aantal)
+{
+    int i = 0;
+    struct Deeltal * deeltallen=malloc(sizeof(struct Deeltal)*aantal);
+    for(i=0;i<aantal;i++)
+    {
+       lees_deeltal(deeltallen+i);                       
+    }
+    return deeltallen;
+}
+
+void schrijf_deeltallen_a(const struct Deeltal * deeltallen,int n)
+{
+    int i=0;
+
+    for(int i=0; i<n; i++)
+    {
+        schrijf_deeltal(deeltallen+i);
+    }   
+}
+
+void schrijf_deeltallen_b(const struct Deeltal * deeltallen,int n)
+{
+    int i=0;
+    int j=0;
+
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\t%d",(deeltallen+i)->waarde,(deeltallen+i)->delers[0]);
+        for(j=1;j<(deeltallen+i)->aantal_delers;j++)
+        {
+            printf("-%d",(deeltallen+i)->delers[j]);
+        }
+        printf("\n");                 
+    }
+}
+
+void schrijf_deeltallen_c(const struct Deeltal * deeltallen, int n)
+{
+    int i = 0;
+    
+    for(i=0;i<n;i++)
+    {
+        schrijf_deeltal(&deeltallen[i]);
+    }
+}
+
+const struct Deeltal * zoek(int waarde, struct Deeltal const * const * ptr, int aantal)
+{
+    int i=0;
+    for(int i=0; i<aantal; i++)
+    {
+        if(waarde==(*(ptr)+i)->waarde)
+            return *(ptr)+i;
+    }
+    return 0;
+}
+
+void release_memory(struct Deeltal * deeltallen, int n)
+{
+    int i=0;
+    
+    for(i=0;i<n;i++)
+    {
+        free(deeltallen[i].delers);
+    }
+}
+
 #endif
