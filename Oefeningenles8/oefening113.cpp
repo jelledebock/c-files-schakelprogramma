1bdf753f7ab788c7c8b1a983a2d3b2a4c8f1dc89
jelledebock <debockjelle@hotmail.com>
2015-11-20 20:53:59 +0100

    some progress exlesson 8

--- Oefeningenles8/oefening113.cpp
+++ Oefeningenles8/oefening113.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include "containers.h"
-
+#define AANTAL 5
 using namespace std;
 
-template<typename T>
-void oef1()
+template<typename T> void oef1()
 {
-	std::vector<T> v{1,4};
-	v.push_back({1,3});
-	schrijf(v);
+
+    std::vector<T> v(3,1);
+    v.push_back(1);
+    v.push_back(3);
+    schrijf(v);
+}
+
+void oef2()
+{
+    queue<string> my_queue;
+    my_queue.push("Een");
+    my_queue.push("Twee");
+    my_queue.push("Drie");
+    schrijf(my_queue);
+    schrijf(my_queue);
+}
+
+void oef3()
+{
+    vector<vector<string> > vector_array(AANTAL,vector<string>(0));
+    vector_array[1].push_back("aap");
+    vector_array[1].push_back("noot");
+    vector_array[1].push_back("mies");
+    schrijf(vector_array);
 }
 
 int main(int argc, char *argv[])
 {
-	oef1();
-	return 0;
+    oef1<int>();
+    oef2();
+    oef3();
+    return 0;
 }
 
 
